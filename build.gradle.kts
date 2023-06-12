860f9f074a38b87a8442e442bff366397a6d41be
Jake Stanger <mail@jstanger.dev>
2023-06-12 21:56:10 +0100

    build: update dep versions

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-    id("org.jetbrains.intellij") version "1.9.0"
+    id("org.jetbrains.intellij") version "1.13.3"
     kotlin("jvm") version "1.7.20"
     java
 }
@@ -13,13 +13,13 @@ repositories {
 
 dependencies {
     implementation(kotlin("stdlib"))
-    testImplementation("org.junit.jupiter:junit-jupiter-api:5.6.0")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.9.0")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine")
 }
 
 // See https://github.com/JetBrains/gradle-intellij-plugin/
 intellij {
-    version.set("2022.2")
+    version.set("2023.1")
 }
 tasks {
     patchPluginXml {
